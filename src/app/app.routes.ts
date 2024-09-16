e460d803f45dcce2a0fd182db18aee9f83d160c1
Ravi Raggu <ravikumartholiya@gmail.com>
2024-09-16 09:13:35 +0530

    adding routes component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { GuestComponent } from './guest/guest.component';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        title: 'My app Hompage',
-        component: AppComponent,
-      },
+    { path: '',   redirectTo: '/guest', pathMatch: 'full' },
     {
         path: "guest",
         title: 'Guest home Page',
         component: GuestComponent
+    },
+    {
+        path: "error", 
+        component: ErrorComponent
     }
 ];
